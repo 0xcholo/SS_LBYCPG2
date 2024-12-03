0f8d7ba12952ac0a316d751c2eee2341569a868c
0xcholo <156637913+0xcholo@users.noreply.github.com>
2024-12-03 14:02:58 +0800

    Add files via upload

--- SS_Db_Conn.php
+++ SS_Db_Conn.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "ss_users";
+    $dbname = "ss_db";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
